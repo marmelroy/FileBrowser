f7ab4a8d968312a30c75ad3d3ea0d888cf232108
Roy Marmelstein <marmelroy@gmail.com>
2016-02-14 21:05:42 +0100

    Cleaning up sample

--- examples/Sample/Sample/ViewController.swift
+++ examples/Sample/Sample/ViewController.swift
@@ -11,34 +11,9 @@ import FileBrowser
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
     @IBAction func showFileBrowser(sender: AnyObject) {
         let fileBrowser = FileBrowser()
-//        fileBrowser.excludesFileTypes = [.ZIP]
-//        fileBrowser.didSelectFile = { (file: File) -> Void in
-//            print("file \(file.filePath)")
-//        }
         self.presentViewController(fileBrowser, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
