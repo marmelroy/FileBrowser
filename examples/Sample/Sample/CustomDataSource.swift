0124669ce68090cc3662b59c3ee05d81c77b9fbf
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-12-30 00:13:25 +0100

    Change Sample CustomDataSource to use JSON file, adapt to changes

--- examples/Sample/Sample/CustomDataSource.swift
+++ examples/Sample/Sample/CustomDataSource.swift
@@ -11,60 +11,52 @@ import FileBrowser
 
 open class CustomDataSource: FileBrowserDataSource {
     
-
+    typealias KeyValue = [String: Any]
+    let json: KeyValue
+    
+    init() {
+        let jsonPath = Bundle.main.path(forResource: "folderContent", ofType: "json")!
+        let data = try! NSData(contentsOfFile: jsonPath, options: []) as Data
+        json = try! JSONSerialization.jsonObject(with: data, options: []) as! KeyValue
+    }
     
     public var excludesFileExtensions: [String]? = nil
     public var excludesFilepaths: [URL]? = nil
     public var excludesWithEmptyFilenames = false
     
+    let rootUrl = URL(string: "/")!
     public var rootDirectory: FBFile {
-        return FBFile(filePath: Directory.root.url())
+        let file = FBFile(path: rootUrl)
+        file.displayName = "Home"
+        return file
     }
     
     let fileManager = FileManager.default
     
-    enum Directory {
-        case root
-        case imagesFolder
-        
-        func url() -> URL {
-            switch self {
-            case .root:
-                return URL(string: "/")!
-            case .imagesFolder:
-                return Directory.root.url().appendingPathComponent("Images", isDirectory: true)
-            }
-        }
-        
-        func contentUrls() -> [URL] {
-            switch self {
-            case .root:
-                return [Directory.imagesFolder.url(),
-                        self.url().appendingPathComponent("Info.plist"),
-                        self.url().appendingPathComponent("Images.zip")]
-            case .imagesFolder:
-                return [self.url().appendingPathComponent("Baymax.jpg"),
-                        self.url().appendingPathComponent("BB8.jpg"),
-                        self.url().appendingPathComponent("Stitch.jpg")]
-            }
-        }
-        
-        func contents() -> [FBFile] {
-            return contentUrls().map(FBFile.init)
-        }
-    }
     
     public func contents(ofDirectory directory: FBFile) throws -> [FBFile] {
-        
-        switch(directory.filePath) {
-        case Directory.root.url():
-            return Directory.root.contents()
-        case Directory.imagesFolder.url():
-            return Directory.imagesFolder.contents()
-        default:
-            return []
+        // traverse the file tree outlined in the JSON file to find the directory
+        let pathComponents = Array(directory.path.pathComponents.dropFirst())
+        let directoryDescription = pathComponents.reduce(json) { currentFolder, subfolderName -> KeyValue in
+            let content = currentFolder["content"]! as! KeyValue
+            return content[subfolderName] as! KeyValue
         }
-
+        
+        let files = (directoryDescription["content"] as! KeyValue).map {name, properties -> FBFile in
+            let properties = properties as! KeyValue
+            let isDirectory = (properties["type"] as? String) == "directory"
+            let path = directory.path.appendingPathComponent(name, isDirectory: isDirectory)
+            let file = FBFile(path: path)
+            if let resourceURLString = properties["location"] as? String, let resourceURL = URL(string: resourceURLString) {
+                file.fileLocation = resourceURL
+            }
+            if let typeName = properties["type"] as? String, let type = FBFileType(rawValue: typeName) {
+                file.type = type
+            }
+            return file
+        }
+        
+        return files
     }
     
     public func attributes(ofItemWithUrl fileUrl: URL) -> NSDictionary? {
@@ -73,10 +65,15 @@ open class CustomDataSource: FileBrowserDataSource {
     
     
     public func dataURL(forFile file: FBFile) throws -> URL {
-        // all the files are contained in the bundle, access them by filename
-        let bundleUrl = Bundle.main.resourceURL!
-        let filename = file.filePath.lastPathComponent
-        return bundleUrl.appendingPathComponent(filename)
+        return file.fileLocation!
     }
     
 }
+
+//extension URL {
+//    func relativeTo(baseUrl: URL) -> URL {
+//        
+//        let keep = self.pathComponents.dropFirst(baseUrl.pathComponents.count)
+//        return keep.fir
+//    }
+//}
