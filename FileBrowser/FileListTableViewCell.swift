be14b15a6dd08a4067ad09e5b0d5c09293ee74fa
msgtrack <msgtrack>
2017-09-29 14:40:37 -0700

    Xcode upgrade to 9

---
+++ FileBrowser/FileListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FileListTableViewCell.swift
+//  FileBrowser
+//
+//
+
+import Foundation
+
+
+class FileListTableViewCell : UITableViewCell
+{
+	// Thumbnail
+	// X: 15, 43 for width and height
+	
+	
+	
+	
+	// Main title file name
+	// X: 73, 43.5 for height
+	
+	// Left Detail 
+	
+	
+	// Right Detail
+	
+	
+	
+}
