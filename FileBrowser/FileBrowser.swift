fdec40a873c1f40aaee7325270dd1e4a88571b3d
iamai <zhigunovep@intersvyaz.net>
2021-11-25 17:21:38 +0500

    add objC

--- FileBrowser/FileBrowser.swift
+++ FileBrowser/FileBrowser.swift
@@ -9,21 +9,21 @@
 import Foundation
 
 /// File browser containing navigation controller.
-open class FileBrowser: UINavigationController {
+@objc open class FileBrowser: UINavigationController {
     
     let parser = FileParser.sharedInstance
     
     var fileList: FileListViewController?
 
     /// File types to exclude from the file browser.
-    open var excludesFileExtensions: [String]? {
+    @objc open var excludesFileExtensions: [String]? {
         didSet {
             parser.excludesFileExtensions = excludesFileExtensions
         }
     }
     
     /// File paths to exclude from the file browser.
-    open var excludesFilepaths: [URL]? {
+    @objc open var excludesFilepaths: [URL]? {
         didSet {
             parser.excludesFilepaths = excludesFilepaths
         }
@@ -48,7 +48,7 @@ open class FileBrowser: UINavigationController {
     ///   - initialPath: NSURL filepath to containing directory.
     ///   - allowEditing: Whether to allow editing.
     ///   - showCancelButton: Whether to show the cancel button.
-    public convenience init(initialPath: URL? = nil, allowEditing: Bool = false, showCancelButton: Bool = true) {
+    @objc public convenience init(initialPath: URL? = nil, allowEditing: Bool = false, showCancelButton: Bool = true) {
         
         let validInitialPath = initialPath ?? FileParser.sharedInstance.documentsURL()
         
